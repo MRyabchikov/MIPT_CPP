f01e0f2e6af9c0aeb1b61fd0dea59dea101d1c7f
MRyabchikov <m.ryabchikov@yandex.ru>
2023-09-12 15:54:29 +0300

    Add fast version

--- class3/fast_pow.cpp
+++ class3/fast_pow.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 double slow_pow (double x, int n)
 {
-    if (n < 0 && x == 0)
+    if (n <= 0 && x == 0)
     {
         throw runtime_error("Zero in non-positive power");
     }
@@ -15,11 +15,32 @@ double slow_pow (double x, int n)
     return ans;
 }
 
+double fast_pow (double x, int n)
+{
+    if (n <= 0 && x == 0)
+    {
+        throw runtime_error("Zero in non-positive power");
+    }
+    else if (n < 0)
+        return fast_pow(1 / x, -n);
+    if (n == 0)
+        return 1;
+    else if (n % 2 == 0)
+    {
+        double tmp = fast_pow(x, n / 2);
+        return tmp * tmp;
+    }
+    return x * fast_pow(x, n - 1);
+}
+
 int main (void)
 {
     cout << slow_pow(7, 0) << endl;
     cout << slow_pow(5, 3) << endl;
     cout << slow_pow(2, -1) << endl;
-    cout << slow_pow(0, -1) << endl;
+    cout << fast_pow(2, 10) << endl;
+    cout << fast_pow(5, 4) << endl;
+    cout << fast_pow(2, -2) << endl;
+    cout << fast_pow(0, -1) << endl;
     return 0;
 }

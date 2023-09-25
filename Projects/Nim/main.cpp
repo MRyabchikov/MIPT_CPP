daeb0953830806dd8f8d53fbe82a78a4252e4c3f
MRyabchikov <m.ryabchikov@yandex.ru>
2023-09-25 18:44:11 +0300

    Add gaming functional

--- Projects/Nim/main.cpp
+++ Projects/Nim/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void print_greetings (void)
@@ -29,7 +30,131 @@ void print_rules (void)
     cout << "- You may quit the game at every moment by entering 0 0 \n";
 }
 
-void game () { return; }
+vector<int> init_field ()
+{
+    vector<int> field{3, 4, 5};
+    return field;
+}
+
+bool game_is_end (const vector<int>& field)
+{
+    for (auto x : field)
+        if (x)
+            return false;
+    return true;
+}
+
+const int player_turn = 1;
+const int bot_turn = 0;
+
+void change_turn (int& current_turn)
+{
+    if (current_turn == player_turn)
+        current_turn = bot_turn;
+    else
+        current_turn = player_turn;
+}
+
+void print_n_char (char c, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << c;
+}
+
+void print_field_condition (const vector<int>& field)
+{
+    cout << "\t\t\tCurrent field condition: \n";
+    for (int i = 0; i < static_cast<int>(field.size()); i++)
+    {
+        print_n_char('*', field[i]);
+        printf("    (%d stones)\n", field[i]);
+    }
+}
+
+bool is_valid_value (const vector<int>& field, int row, int ammount)
+{
+    return row > 0 && row <= static_cast<int>(field.size()) &&
+           ammount <= field[row - 1] && ammount > 0;
+}
+
+int handle_player_turn (vector<int>& field)
+{
+    while (true)
+    {
+        cout << "Enter your turn: ";
+        int row, ammount;
+        cin >> row >> ammount;
+        if (!cin)
+        {
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        if (row == 0 && ammount == 0)
+            return 0;
+        if (is_valid_value(field, row, ammount))
+        {
+            field[row - 1] -= ammount;
+            return 1;
+        }
+    }
+}
+
+int calculate_nim_sum (const vector<int>& field)
+{
+    int res = field[0];
+    for (int i = 1; i < static_cast<int>(field.size()); i++)
+        res ^= field[i];
+    return res;
+}
+
+void make_random_turn (vector<int>& field)
+{
+    int row = rand() % field.size();
+    int ammount = rand() % field[row];
+    field[row] -= ammount;
+}
+
+void handle_bot_turn (vector<int>& field)
+{
+    if (calculate_nim_sum(field) == 0)
+        make_random_turn(field);
+    else
+        for (int i = 0; i < static_cast<int>(field.size()); i++)
+            for (int j = 1; j < field[i]; j++)
+            {
+                field[i] -= j;
+                if (calculate_nim_sum(field) == 0)
+                    return;
+                else
+                    field[i] += j;
+            }
+}
+
+void game ()
+{
+    vector<int> field = init_field();
+    int current_turn = rand() % 2 ? player_turn : bot_turn;
+    while (!game_is_end(field))
+    {
+        print_field_condition(field);
+        if (current_turn == player_turn)
+        {
+            if (handle_player_turn(field) == 0)
+            {
+                cout << "Game over with your capitulation\n";
+                return;
+            }
+            else
+                handle_bot_turn(field);
+        }
+        change_turn(current_turn);
+    }
+    if (current_turn == bot_turn)
+        cout << "Congratulations! You beat the machine!\n";
+    else
+        cout << "Unfortunately you lose. Try again!\n";
+    return;
+}
 
 int main (void)
 {

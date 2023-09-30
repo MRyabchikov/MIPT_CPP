fd291ad88686b3fb6afdf75795a95f0527eee3c6
MRyabchikov <m.ryabchikov@yandex.ru>
2023-09-30 11:06:13 +0300

    Add allmost all for vector

---
+++ SeminarsWork/class6/vector/vector2d.cpp
@@ -0,0 +1,48 @@
+#include "vector2d.h"
+#include <iostream>
+using namespace std;
+
+Vector2d operator+ (const Vector2d& v1, const Vector2d& v2)
+{
+    return Vector2d(v1.getX() + v2.getX(), v1.getY() + v2.getY());
+}
+
+Vector2d operator+= (Vector2d& v1, const Vector2d& v2)
+{
+    v1 = v1 + v2;
+    return v1;
+}
+
+Vector2d operator- (const Vector2d& v1) { return Vector2d(-v1.getX(), -v1.getY()); }
+
+Vector2d operator- (const Vector2d& v1, const Vector2d& v2) { return v1 + (-v2); }
+
+Vector2d operator-= (Vector2d& v1, const Vector2d& v2)
+{
+    v1 = v1 - v2;
+    return v1;
+}
+
+ostream& operator<< (ostream& fout, const Vector2d& v1)
+{
+    fout << v1.getX() << " " << v1.getY();
+    return fout;
+}
+
+istream& operator>> (istream& fin, Vector2d& v1)
+{
+    double x, y;
+    fin >> x >> y;
+    v1.setX(x);
+    v1.setY(y);
+    return fin;
+}
+
+int main (void)
+
+{
+    Vector2d v1, v2;
+    cin >> v1 >> v2;
+    v1 -= v2;
+    cout << v1;
+}
---
+++ SeminarsWork/class6/vector/vector2d.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Vector2d
+{
+  public:
+    Vector2d(double newX = 0, double newY = 0) : x{newX}, y{newY} {}
+
+    double getX () const { return x; }
+
+    double getY () const { return y; }
+
+    void setX (double newX) { x = newX; }
+
+    void setY (double newY) { y = newY; }
+
+  private:
+    double x, y;
+};
+
+Vector2d operator+ (const Vector2d& v1, const Vector2d& v2);
+Vector2d operator+= (Vector2d& v1, const Vector2d& v2);
+Vector2d operator- (const Vector2d& v1);
+Vector2d operator- (const Vector2d& v1, const Vector2d& v2);
+Vector2d operator-= (Vector2d& v1, const Vector2d& v2);
+ostream& operator<< (ostream& fout, const Vector2d& v1);
+istream& operator>> (istream& fin, Vector2d& v1);

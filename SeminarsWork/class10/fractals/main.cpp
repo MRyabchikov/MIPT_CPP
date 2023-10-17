678e73217a2d823a98ed5add988d24cd301eda00
MRyabchikov <m.ryabchikov@yandex.ru>
2023-10-17 17:24:33 +0300

    Unwoking dragons

--- SeminarsWork/class10/fractals/main.cpp
+++ SeminarsWork/class10/fractals/main.cpp
@@ -22,6 +22,14 @@ void append (Closed_polyline& poly, const list<Vector2d>& points)
     }
 }
 
+void append (Open_polyline& poly, const list<Vector2d>& points)
+{
+    for (const auto& point : points)
+    {
+        poly.add(as_point(point));
+    }
+}
+
 void koch_fractal_step (list<Vector2d>& curve)
 {
     if (curve.begin() == curve.end())
@@ -35,6 +43,19 @@ void koch_fractal_step (list<Vector2d>& curve)
         curve.insert(curr, p);
         curve.insert(curr, m);
         curve.insert(curr, q);
+        prev = curr;
+    }
+}
+
+void dragon_fractal_step (list<Vector2d>& curve)
+{
+    if (curve.begin() == curve.end())
+        throw runtime_error("empty curve");
+    auto prev = --curve.end();
+    for (auto curr = curve.begin(); curr != curve.end(); curr++)
+    {
+        auto p = (*curr).rotate(pi / 4, *prev);
+        curve.insert(curr, p);
         prev = curr;
     }
 }
@@ -68,7 +89,32 @@ void draw_koch_snowflake (int w, int n)
         append(curve, ngon);
         curve.set_color(Color::blue);
         koch_fractal_step(ngon);
-        is_fine = false;
+        is_fine = max_edge_len(ngon) < 1.0;
+        if (is_fine)
+            n_steps.set_color(Color::red);
+        win.attach(curve);
+        win.wait_for_button();
+        win.detach(curve);
+        n_steps.set_label(to_string(stoi(n_steps.label()) + 1));
+    }
+}
+
+void draw_dragon (int w, int n)
+{
+    Simple_window win{Point(100, 100), w, w, "Dragon"};
+    Vector2d c{w / 2., w / 2.};
+    double r = {0.85 * w / 2.};
+    auto ngon = regular_poligon_without_one_vertexe(n, c, r, -pi / 2);
+    Text n_steps = {as_point(c), "0"};
+    n_steps.set_color(Color::blue);
+    win.attach(n_steps);
+    for (bool is_fine = false; !is_fine;)
+    {
+        Open_polyline curve;
+        append(curve, ngon);
+        curve.set_color(Color::blue);
+        dragon_fractal_step(ngon);
+        is_fine = max_edge_len(ngon) < 1.0;
         if (is_fine)
             n_steps.set_color(Color::red);
         win.attach(curve);
--- lib/polygon/poly.h
+++ lib/polygon/poly.h
@@ -4,5 +4,6 @@
 using namespace std;
 
 list<Vector2d> regular_polygon (int n, Vector2d center, double radius, double phi_0);
+list<Vector2d> regular_poligon_without_one_vertexe (int n, Vector2d center, double radius, double phi_0);
 
 Vector2d lerp (Vector2d a, Vector2d b, double t);

94bceb06d98f080df7a9ad41bcce3e77e68088f5
MRyabchikov <m.ryabchikov@yandex.ru>
2023-12-15 22:42:18 +0300

    Pre-final version

--- Projects/Vector/main.cpp
+++ Projects/Vector/main.cpp
@@ -1,4 +1,3 @@
-#include "vector.cpp"
 #include "vector.h"
 #include <iostream>
 #include <memory>
@@ -7,22 +6,48 @@ using namespace std;
 
 template <typename T> ostream& operator<< (ostream& fout, const Vector<T>& v)
 {
+    TRACE_FUNC;
     for (int i = 0; i < v.size(); i++)
         fout << v[i] << " ";
     return fout;
 }
 
+Vector<double> init_vec ()
+{
+    TRACE_FUNC;
+    return {1.4, 2.1, 3.9};
+}
+
 int main (void)
 {
     TRACE_FUNC;
-    Vector<int> a{0, 1, 2, 3};
     Vector<Vector<int>> v1;
+    cout << "Default initialization: " << v1 << endl;
     Vector<int> v2 = {1, 2, 3};
-    cout << v2 << endl;
-    for (int i = 0; i < 3; i++)
+    cout << "Initializer list inititalization: " << v2 << endl;
+    Vector<double> v3(4);
+    cout << "Initialization with size 4: " << v3 << endl;
+    Vector<double> v4(v3.size());
+    cout << "Intitialization with copy constructor: " << v4 << endl;
+    Vector<double> v5(move(init_vec()));
+    cout << "Inititalization with moving constructor: " << v5 << endl;
+    Vector<int> v6;
+    v6 = v2;
+    cout << "Now v6 is equal to v2: " << v6 << endl;
+    for (int i = 0; i < 4; i++)
         v2.push_back(i + 4);
-    cout << v2;
-    // v1.push_back(v2);
-    // v1.push_back(v2);
+    cout << "v2 after pushing back some values: " << v2 << endl;
+    v1.push_back(v2);
+    v1.push_back(v2);
+    cout << "Vector of vectors: " << v1 << endl;
+    cout << "v2 size: " << v2.size() << "; v2 capacity: " << v2.capacity() << endl;
+    try
+    {
+        cout << v1.at(3);
+    }
+    catch (out_of_range&)
+    {
+        cout << "Blocked index error" << endl;
+    }
     return 0;
 }
